05b857aa1978f0010313188ea7fe4f8850738326
Tim <48717893+timyers@users.noreply.github.com>
2024-08-01 10:35:26 -0400

    update with 32 snps subset

--- R/fabian_format_maker2.R
+++ R/fabian_format_maker2.R
@@ -5,12 +5,14 @@
 library(dplyr)
 
 # Read the first file
-snp_file1 <- "/Users/myersta/Library/CloudStorage/OneDrive-NationalInstitutesofHealth/Winter/plots/fabian_tf/required_format/fabian_format_all_39_SNPs_20240723_144047.txt"
+# snp_file1 <- "/Users/myersta/Library/CloudStorage/OneDrive-NationalInstitutesofHealth/Winter/plots/fabian_tf/required_format/fabian_format_all_39_SNPs_20240723_144047.txt"
+snp_file1 <- "data/input/fabian_tffm/fabian_format_32_SNPs_20240801_101111.txt"
 file1 <- read.table(snp_file1, header = TRUE, sep = "\t")
 
 # Read the second file
-snp_file2 <- "/Users/myersta/Library/CloudStorage/OneDrive-NationalInstitutesofHealth/Winter/plots/fabian_tf/six_snps_fabian_format.txt"
-file2 <- read.table("/mnt/data/six_snps_fabian_format.txt", header = TRUE, sep = "\t")
+# snp_file2 <- "/Users/myersta/Library/CloudStorage/OneDrive-NationalInstitutesofHealth/Winter/plots/fabian_tf/six_snps_fabian_format.txt"
+# snp_file2 <- "data/input/fabian_tffm/six_snps_fabian_format.txt"
+# file2 <- read.table(snp_file2, header = TRUE, sep = "\t")
 
 # Adjust the first file to match the columns and order of the second file
 file1_reformatted <- file1 %>%
@@ -19,6 +21,11 @@ file1_reformatted <- file1 %>%
   mutate(chrom = as.integer(gsub("chr", "", chrom)))
 
 # Write the reformatted file to a new file
-output_path <- "/Users/myersta/Library/CloudStorage/OneDrive-NationalInstitutesofHealth/Winter/plots/fabian_tf/required_format/"
-output_file <- paste0(output_path, "reformated_fabian_all_39_SNPs.txt")
+# output_path <- "/Users/myersta/Library/CloudStorage/OneDrive-NationalInstitutesofHealth/Winter/plots/fabian_tf/required_format/"
+output_path <- "data/input/fabian_tffm/"
+output_file <- paste0(output_path, "reformatted_fabian_all_39_SNPs.txt")
+output_file <- paste0(output_path,
+                      "reformatted_fabian_",
+                      nrow(file1_reformatted),
+                      "_SNPs.txt")
 write.table(file1_reformatted, output_file, sep = "\t", row.names = FALSE, quote = FALSE)
